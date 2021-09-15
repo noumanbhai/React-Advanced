4382ccc1c98fdb0b3792a3f77fa3322e1bb4f17b
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-16 00:23:58 +0500

    Counter increment decrement reset

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import ErrorExample from './tutorial/1-useState/setup/4-useState-object'
+import React from "react";
+import ErrorExample from "./tutorial/1-useState/setup/5-useState-counter";
+
 function App() {
   return (
-    <div className='container'>
-      <ErrorExample/>
+    <div className="container">
+      <ErrorExample />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [count, setcount] = useState(0);
+
+  const Increment = () => {
+    setcount(count + 1);
+  };
+  const Decrement = () => {
+    setcount(count - 1);
+  };
+  const Reset = () => {
+    setcount(0);
+  };
+
+  return (
+    <div>
+      <h2>{count} </h2>
+      <button className="btn" onClick={Increment}>
+        Increment
+      </button>
+
+      <button className="btn" onClick={Decrement}>
+        Decrement
+      </button>
+
+      <button className="btn" onClick={Reset}>
+        Resset
+      </button>
+    </div>
+  );
 };
 
 export default UseStateCounter;
