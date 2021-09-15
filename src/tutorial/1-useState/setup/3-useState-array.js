9bbe5237a09a39f3d0a930a7f8b437aecc66a557
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-15 19:07:40 +0500

    Remove item in array use by filter

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,19 +3,30 @@ import { data } from '../../../data';
 
 const UseStateArray = () => {
   const [people,setPeople]=React.useState(data)
+
+const remove=(id)=>{
+
+  let newPeople=people.filter((data)=>data.id !==id);
+         setPeople(newPeople);
+}
+
   return(<div>
 
 {
   people.map((person)=>{
     const {id,name}=person;
+    
 return( 
 <div key={id} className='item'>
   <h3>{id}={name}</h3>
+    <button className='btn' onClick={()=>remove(id)}>
+    Clear single Item
+  </button>
   </div>
   );
   })}
-  <button className='btn' onClick={()=> setPeople([])}>
-    Clear Item
+  <button className='btn' onClick={()=>setPeople()}>
+    Clear all Item
   </button>
 </div>
   );
