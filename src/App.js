79202852f05f1bb06fcf085a5148ec1fc3019ee9
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-15 16:21:17 +0500

    git React.fregment is use for the parent dev

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ErrorExample from './tutorial/1-useState/setup/1-error-example'
+function App() {
+  return (
+    <div className='container'>
+      <ErrorExample/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ErrorExample = () => {
+  let title='This is React cource';
+  return (
+    <div>
+  <h1>{title}</h1></div>
+  )};
+
+export default ErrorExample;
