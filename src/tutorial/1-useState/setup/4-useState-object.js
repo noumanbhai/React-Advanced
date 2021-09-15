c2ac220db21395253d88c95ae32e87e00f5fc576
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-15 20:04:34 +0500

    object value change by useState

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
 
+
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [Person, setPerson] = useState({
+   id: 1,
+   name: 'Kamran' ,
+   age:22,
+   message:"This is a custom message"
+
+
+
+
+
+  })
+console.log(Person);
+  const changeMessage=()=>{
+
+setPerson({
+   id: 2,
+   name: 'Nouman' ,
+   age:28,
+   message:"This is Update message"
+}
+)
+}
+  return (<>
+  <h1>useState object example</h1>
+<h3>{Person.id}</h3>
+<h3>{Person.name}</h3>
+<h3>{Person.age}</h3>
+<h3>{Person.message}</h3>
+<button className='btn' onClick={changeMessage}>Change Message</button>
+</>);
 };
 
 export default UseStateObject;
