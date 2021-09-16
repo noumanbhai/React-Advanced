db4acfb781d29dfe8f59d95fa1ec1605e4583404
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-16 18:55:30 +0500

    json data api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ErrorExample from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
+import ErrorExample from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 
 function App() {
   return (
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://jsonplaceholder.typicode.com/posts";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setusers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setusers(users);
+    // console.log("users ==>>>", users);
+  };
+  useEffect(() => {
+    getUsers();
+    // return () => {
+    //   cleanup;
+    // };
+  }, []);
+  // debugger;
+  return (
+    <div>
+      <h2>fetch data</h2>
+      <ul className="users">
+        {users.map((user) => {
+          const { userId, id, title, body } = user;
+          return (
+            <div>
+              <li key={id}>
+                <div>
+                  <h3>{id}</h3>
+                  <h4>
+                    <a href="">{title}</a>
+                  </h4>
+                  <h5>{body}</h5>
+                </div>
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default UseEffectFetchData;
