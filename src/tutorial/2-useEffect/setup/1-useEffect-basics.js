931c97d33fe5468c2356378fb3dd8837af9f066b
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-16 13:07:28 +0500

    useEffect document.title value change

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [count, setcount] = useState(0);
+  useEffect(() => {
+    console.log("First");
+    document.title = `${count}`;
+  });
+  console.log("lst");
+  return (
+    <>
+      <h2>useEffect Basics</h2>
+      <h2>{count}</h2>
+      <button className="btn" onClick={() => setcount(count + 1)}>
+        Click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
