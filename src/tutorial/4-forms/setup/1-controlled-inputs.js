4da6f3f86630ef69aafc1bb3c14cf669a2d524f5
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-20 15:12:56 +0500

    Add form insert and view using useState

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,64 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [people, setpeople] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name && email) {
+      const person = { id: new Date().getTime().toString(), name, email };
+      // console.log(person);
+      setpeople((people) => {
+        return [...people, person];
+      });
+      setname("");
+      setemail("");
+    } else {
+      console.log("form is not submit");
+    }
+  };
+
+  return (
+    <div>
+      <h1>controlled inputs</h1>;
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name :</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={name}
+              onChange={(e) => setname(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email :</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
+            />
+          </div>
+          <button type="submit">Add Person</button>
+        </form>
+        {people.map((person, index) => {
+          const { id, name, email } = person;
+
+          return (
+            <div key={id} className="item">
+              <p>Youe Name:{name}</p>
+              <p>Your Email:{email}</p>
+            </div>
+          );
+        })}
+      </article>
+    </div>
+  );
 };
 
 export default ControlledInputs;
