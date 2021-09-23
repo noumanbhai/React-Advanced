f6930884e7f9cd1960e2455c6539f39ed7368edc
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-23 13:18:08 +0500

    image hangle by api call simple method

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+// import PropTypes from "prop-types";
+// import defaultImage from "../../../assets/default-image.jpeg";
+const Product = ({ image, name, price }) => {
+  console.log(image, price, name);
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+  return (
+    <article className="product">
+      <img src={image?.url} alt={name} />
+      <h4>{name}</h4>
+      <h4>Rs:{price}</h4>
+    </article>
+  );
 };
 
+// Product.propTypes = {
+//   image: PropTypes.object.isRequired,
+//   name: PropTypes.string.isRequired,
+//   price: PropTypes.number.isRequired,
+// };
+// Product.defaultProps = {
+//   name: "default name",
+//   price: 5.5,
+//   image: "default Image",
+// };
 export default Product;
